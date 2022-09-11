2f2158cf24992e0e8e9144df6f6aedd4e9c9fc23
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-09-11 23:54:42 +0300

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@ User.create(username:"Dentric", password: "12345")
 
 #stocks data
 
-Stock.create(title:"Sugar", amount: "Three bags")
-Stock.create(title:"Flour", amount: "4 kgs")
-Stock.create(title:"Bread", amount: "four crates"  )
-Stock.create(title:"Ketchup", amount: "Six packets")
+Stock.create(title:"Sugar", description: "Three bags")
+Stock.create(title:"Flour", description: "four kgs")
+Stock.create(title:"Bread", description: "four crates"  )
+Stock.create(title:"Ketchup", description: "Six packets")
 
 puts "✅ Done seeding!"
